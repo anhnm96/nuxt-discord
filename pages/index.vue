af4e2145368f5ef164c554e6122d1cc6c9f99985
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-11-30 20:49:28 +0900

    make panels scrollable

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="flex h-screen text-white">
-    <div class="bg-gray-800 p-4">
+    <div class="space-y-2 overflow-y-scroll bg-gray-800 p-3">
       <div
+        v-for="(_, i) in [...Array(40)]"
+        :key="i"
         class="flex h-12 w-12 items-center justify-center rounded-full bg-white text-gray-800"
       >
-        TW
+        {{ i }}
       </div>
     </div>
+
     <div class="flex w-60 flex-col bg-gray-700">
-      <div class="p-4 shadow-md">Tailwind CSS</div>
-      <div class="flex-1 p-4">channels</div>
+      <div class="flex h-12 items-center px-3 shadow-md">Tailwind CSS</div>
+      <div class="flex-1 space-y-2 overflow-y-scroll p-3">
+        <p v-for="(_, i) in [...Array(40)]" :key="i">channel {i}</p>
+      </div>
     </div>
+
     <div class="flex flex-1 flex-col bg-gray-600">
-      <div class="p-4 shadow-md">general</div>
-      <div class="flex-1 p-4">messages</div>
+      <div class="flex h-12 items-center px-3 shadow-md">general</div>
+      <div class="flex-1 space-y-4 overflow-y-scroll p-3">
+        <p v-for="(_, i) in [...Array(40)]" :key="i">
+          Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Vel saepe laudantium sed reprehenderit incidunt! Hic rem quos
+          reiciendis, fugit quae ratione beatae veniam laborum voluptatem, iusto
+          dolorum, voluptates suscipit quia.
+        </p>
+      </div>
     </div>
   </div>
 </template>
