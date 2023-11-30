1d60a73f8207769c6a8d840a97bc9139ba839e77
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-11-30 21:02:07 +0900

    customize colors

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex h-screen text-white">
-    <div class="space-y-2 overflow-y-scroll bg-gray-800 p-3">
+  <div class="flex h-screen text-gray-100">
+    <div class="space-y-2 overflow-y-scroll bg-gray-900 p-3">
       <div
         v-for="(_, i) in [...Array(40)]"
         :key="i"
@@ -10,14 +10,16 @@
       </div>
     </div>
 
-    <div class="flex w-60 flex-col bg-gray-700">
+    <div class="flex w-60 flex-col bg-gray-800">
       <div class="flex h-12 items-center px-3 shadow-md">Tailwind CSS</div>
-      <div class="flex-1 space-y-2 overflow-y-scroll p-3">
+      <div class="flex-1 space-y-2 overflow-y-scroll p-3 text-gray-300">
+        <p class="text-white">channel (unread)</p>
+        <p class="text-white">channel (unread)</p>
         <p v-for="(_, i) in [...Array(40)]" :key="i">channel {i}</p>
       </div>
     </div>
 
-    <div class="flex flex-1 flex-col bg-gray-600">
+    <div class="flex flex-1 flex-col bg-gray-700">
       <div class="flex h-12 items-center px-3 shadow-md">general</div>
       <div class="flex-1 space-y-4 overflow-y-scroll p-3">
         <p v-for="(_, i) in [...Array(40)]" :key="i">
