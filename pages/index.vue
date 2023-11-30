5a4fd6359590194c2a6efdf85cb079c2ef408835
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-11-30 20:40:06 +0900

    scaffold layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,19 @@
 <template>
-  <div
-    class="flex min-h-screen items-center justify-center bg-gray-700 text-white"
-  >
-    <div class="max-w-lg">
-      <div class="flex px-4 py-1 hover:bg-gray-800 hover:bg-opacity-30">
-        <img
-          class="mr-4 h-10 w-10 rounded-full"
-          src="/adamwathan.jpeg"
-          alt=""
-        />
-        <div>
-          <p class="flex items-baseline">
-            <span class="mr-2 text-sm font-medium text-green-500">
-              adamwathan
-            </span>
-            <span class="text-xs text-gray-500">01/15/2021</span>
-          </p>
-          <p class="text-gray-300">
-            You should never use something like leading relaxed with a big font
-            size, it goes against all typography best practices. Line height
-            should decrease as font size gets bigger
-          </p>
-        </div>
+  <div class="flex h-screen text-white">
+    <div class="bg-gray-800 p-4">
+      <div
+        class="flex h-12 w-12 items-center justify-center rounded-full bg-white text-gray-800"
+      >
+        TW
       </div>
-      <div class="mt-1 px-4 py-1 hover:bg-gray-800 hover:bg-opacity-30">
-        <p class="pl-14 text-gray-300">
-          You can override it in your config if you want but ultimately we chose
-          the defaults they did because they let you get results closest to what
-          a professional designer would do more easily
-        </p>
-      </div>
-      <div class="mt-1 px-4 py-1 hover:bg-gray-800 hover:bg-opacity-30">
-        <p class="pl-14 text-gray-300">
-          Since we changed this in tailwind 2 I’ve almost never used a leading
-          class at all
-        </p>
-      </div>
+    </div>
+    <div class="flex w-60 flex-col bg-gray-700">
+      <div class="p-4 shadow-md">Tailwind CSS</div>
+      <div class="flex-1 p-4">channels</div>
+    </div>
+    <div class="flex flex-1 flex-col bg-gray-600">
+      <div class="p-4 shadow-md">general</div>
+      <div class="flex-1 p-4">messages</div>
     </div>
   </div>
 </template>
