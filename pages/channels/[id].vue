0f29c2966136f9f9b112d969871012c9e0e64899
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-12-02 17:36:04 +0900

    style the channel list

--- pages/channels/[id].vue
+++ pages/channels/[id].vue
@@ -9,11 +9,42 @@
       Tailwind CSS
       <Icon class="ml-auto" size="18px" name="carbon:chevron-down" />
     </button>
-    <div
-      class="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300"
-    >
-      <p class="text-white">general</p>
-      <p v-for="(_, i) in [...Array(40)]" :key="i">channel {{ i }}</p>
+    <!-- channel list -->
+    <div class="mt-[17px] flex-1 overflow-y-scroll font-medium text-gray-300">
+      <div class="space-y-0.5">
+        <a
+          href="#"
+          class="hover:bg-gray-550/[0.16] group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:text-gray-100"
+        >
+          <Icon
+            class="mr-1.5 text-gray-400"
+            size="20px"
+            name="ic:baseline-library-books"
+          />
+          welcome
+          <Icon
+            class="ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100"
+            size="16px"
+            name="material-symbols:person-add"
+          />
+        </a>
+        <a
+          href="#"
+          class="hover:bg-gray-550/[0.16] group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:text-gray-100"
+        >
+          <Icon
+            class="mr-1.5 text-gray-400"
+            size="20px"
+            name="heroicons-solid:speakerphone"
+          />
+          announcements
+          <Icon
+            class="ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100"
+            size="16px"
+            name="material-symbols:person-add"
+          />
+        </a>
+      </div>
     </div>
   </div>
 
