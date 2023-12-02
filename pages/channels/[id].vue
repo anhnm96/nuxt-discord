86056f275520ab2aa6a9a0ac335a9d33ba8e060c
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-12-02 17:12:16 +0900

    make the server header

--- pages/channels/[id].vue
+++ pages/channels/[id].vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex w-60 flex-col bg-gray-800">
-    <div
-      class="flex h-12 items-center px-3 font-title font-semibold text-white shadow-md"
+    <button
+      class="hover:bg-gray-550/[0.16] flex h-12 items-center px-4 font-title text-[15px] font-semibold text-white shadow-sm transition"
     >
-      Server {{ $route.params.id }}
-    </div>
+      <div class="mr-2 flex items-center">
+        <Icon size="16px" name="material-symbols:verified-rounded" />
+      </div>
+      Tailwind CSS
+      <Icon class="ml-auto" size="18px" name="carbon:chevron-down" />
+    </button>
     <div
       class="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300"
     >
@@ -14,7 +18,7 @@
   </div>
 
   <div class="flex flex-1 flex-col bg-gray-700">
-    <div class="flex h-12 items-center px-3 shadow-md">general</div>
+    <div class="flex h-12 items-center px-3 shadow-sm">general</div>
     <div class="flex-1 space-y-4 overflow-y-scroll p-3">
       <p v-for="(_, i) in [...Array(40)]" :key="i">
         Message {{ i }}. Lorem ipsum dolor sit amet consectetur adipisicing
