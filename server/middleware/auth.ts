e6faff991a4895e3936e56c654e2ea13da5880c0
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-12-10 22:15:41 +0900

    auth middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+export default defineEventHandler((event) => {
+  // protect all routes except /api/auth
+  if (event.node.req.url?.startsWith('/api/auth')) return
+
+  const config = useRuntimeConfig(event)
+  const [_, token] = getHeader(event, 'authorization')?.split(' ') ?? []
+  if (!token)
+    throw createError({ statusMessage: 'Unauthenticated', statusCode: 401 })
+
+  try {
+    const payload = jwt.verify(token, config.secret)
+    event.context.auth = payload
+  } catch (err: any) {
+    if (err.name === 'TokenExpiredError') {
+      // TODO: handle token expired
+    }
+    throw createError({ statusMessage: 'Unauthenticated', statusCode: 401 })
+  }
+})
