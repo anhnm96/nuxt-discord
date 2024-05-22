d2fc2b1ad793d17bf6d200d91d5d888a86be1fbf
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-05-22 11:24:17 +0700

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
